a46a25dd6a5c1eda68ca98653912600e584b6fe3
melihcakirtas <57617333+melihcakirtas@users.noreply.github.com>
2021-02-04 08:49:54 +0300

    check net connection

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 from pytube import YouTube
 from moviepy.editor import * 
 import os,shutil
+import requests
+import requests
 
 def get_mp3():
     url = input("Lütfen Youtube linkini giriniz : ")
     output = input("Hangi formatta indirmek istersiniz ? (wav/mp3) : ")
     print("Converting....")
 
-    mp4= YouTube(url).streams.get_highest_resolution().download()
-    mp3 = mp4.split(".mp4",1)[0] + f".{output}"
+    if output == "wav" :
+        mp4= YouTube(url).streams.get_highest_resolution().download()
+        mp3 = mp4.split(".mp4",1)[0] + f".{output}"
 
-    video_clip = VideoFileClip(mp4)
-    audio_clip = video_clip.audio
-    audio_clip.write_audiofile(mp3)
+        video_clip = VideoFileClip(mp4)
+        audio_clip = video_clip.audio
+        audio_clip.write_audiofile(mp3)
 
-    audio_clip.close()
-    video_clip.close()
+        audio_clip.close()
+        video_clip.close()
+        os.remove(mp3)
+    else :
+         mp4= YouTube(url).streams.get_highest_resolution().download()
+         mp3 = mp4.split(".mp4",1)[0] + f".{output}"
 
-    #if you want to save the mp4 format,you need to delete it
-    os.remove(mp4) 
-    
+         video_clip = VideoFileClip(mp4)
+         audio_clip = video_clip.audio
+         audio_clip.write_audiofile(mp3)
 
-get_mp3()    
+         audio_clip.close()
+         video_clip.close()  
+         #if you want to save the mp4 format,you need to delete it
+         os.remove(mp4)
+     
+
+try:
+    if requests.get('https://google.com').ok:
+        print("You're Online")
+        get_mp3()
+except:
+    print("You're Offline")    
+
